35a075ebc8264f08d53adc156e1cd3874348e26e
Claudiu Constantin Bogdan <bogdan.claudiu93@gmail.com>
2018-11-04 22:10:06 +0200

    Update ChangeExpenser.js

--- ChangeExpenser.js
+++ ChangeExpenser.js
@@ -33,16 +33,25 @@ function checkCashRegister(price, cash, cid) {
  let availableMoney = availableChange(cid, minChange);
   
   let mCHange = giveChange(brutChange, availableMoney, []);
-  console.log(mCHange);
+  //console.log(mCHange);
   // Here is your change, ma'am.
-  const statusDic = [
-    {status: "INSUFFICIENT_FUNDS", change: []},
-    {status: "CLOSED", change: []},
-    {status: "OPEN", change: []}
-  ];
+  let changeRes = 'Hellow';
+  console.log(changeRes);
+  console.log(mCHange);
+  console.log(`Test`);
+  if(mCHange.newChange == 0){
+    if(false){
 
-  let changeRes = statusDic[2];
-  changeRes.change = mCHange;
+    }
+    else{
+changeRes = {status: "OPEN", change: mCHange.result} ;
+    }
+    
+  } 
+  if(mCHange.newChange > 0){
+    changeRes = {status: "INSUFFICIENT_FUNDS", change: []} ;
+  }  
+  console.log(changeRes)
   return changeRes;
 }
 
@@ -57,7 +66,7 @@ function giveChange(change, availableMoney, result){
 
   //console.log(newChange);
   if(newChange === 0 || availableMoney.length == 0){
-    return result;
+    return {result, newChange};
   }
   else{
     return giveChange(newChange, availableMoney, result);
@@ -103,5 +112,4 @@ function getChangeValue(name){
   }
   return null;
 }
-
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
